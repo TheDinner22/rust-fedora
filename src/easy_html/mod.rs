7523f8ffad9a4c4c612803874b25e6a9d9dbe9b4
Joe Goodman <79279956+TheDinner22@users.noreply.github.com>
2022-12-23 15:26:38 -0500

    began parsing http request

--- src/easy_html/mod.rs
+++ src/easy_html/mod.rs
@@ -1,4 +1,32 @@
 // types which make working with html requests and responses bearable
+
+use std::rc::Rc;
+use std::collections::HashMap;
+
+enum Method {
+    Post,
+    Get,
+    Put,
+    Delete
+}
+
+impl Method {
+    fn try_from<T>(value: T) -> Result<Self, String>
+    where
+        T: ToString
+    {
+        let val = value.to_string().trim().to_lowercase();
+
+        match val.as_str() {
+            "post" => Ok(Method::Post),
+            "get" => Ok(Method::Get),
+            "put" => Ok(Method::Put),
+            "delete" => Ok(Method::Delete),
+            _ => Err(String::from("invald http method"))
+        }
+    }
+}
+
 pub struct Response {
     status_code: u16, // todo is u32 the right size??
     headers: String, // todo is it type String???
@@ -20,10 +48,86 @@ pub struct Request<'a> {
     http_ver: &'a str // todo is it type String???
 }
 
+impl<'a> Request<'a> {
+    fn parse_method(method_str: &str) -> Result<Method, String> {
+        Method::try_from(method_str)
+    }
+
+    fn parse_url(url_string: &str) -> Result<(&str, String), String> {
+        let mut url_iter = url_string.split("?");
+        
+        let path = match url_iter.next() {
+            Some(string) => if string.is_empty() { "/" } else { string },
+            None => return Err("invalid http request".to_string()),
+        };
+
+        let raw_query_params: String = url_iter.collect();
+
+        Ok((path, raw_query_params))
+    }
+
+    fn parse_query_string(query_params: String) -> Option<HashMap<String, String>> {
+        if query_params.is_empty() { return None;}
+
+        let query_map: HashMap<String, String> = query_params
+            .split("&")
+            .filter_map(|pair| pair.split_once("="))
+            .map(|(key, value)| (key.to_owned(), value.to_owned()))
+            .collect();
+
+        Some(query_map) // todo could this accidentally be empty?
+    }
+}
+
 impl<'a> TryFrom<Vec<u8>> for Request<'a> {
-    type Error;
+    type Error = String;
 
     fn try_from(value: Vec<u8>) -> Result<Self, Self::Error> {
-        todo!()
+        let http_string = match String::from_utf8(value) {
+            Ok(request) => request,
+            Err(e) => return Err(e.to_string()),
+        };
+
+        let lines: Vec<&str> = http_string.split("\r\n").collect();
+        let first_line = *lines.first().unwrap_or(&"");
+
+        // if the first line is empty, the request is bad!
+        if first_line.is_empty() { return Err("invalid http request".to_string()) }
+
+        let mut first_line_words = first_line.split_whitespace();
+
+        // parse method
+        let method = match first_line_words.next() {
+            Some(string) => Request::parse_method(string)?,
+            None => return Err("invalid http request".to_string()),
+        };
+        
+        // parse url to get the path and the query parameters (if any)
+        // todo this assumes urls cannot contain "?" character (it should only be used for query string stuff)
+        let (raw_path, raw_query_string) = match first_line_words.next() {
+            Some(string) => Request::parse_url(string)?,
+            None => return Err("invalid http request".to_string()),
+        };
+
+        // further parse the query params into an Option<hashmap>
+        let query_params = Request::parse_query_string(raw_query_string); 
+
+        // parse header
+
+        // parse http ver as x where version is 1.x
+
+        // parse body
+
+        Ok(Request { body: "", headers: "", query_string_object: "", path: "", method: "", http_ver: "" })
     }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        let result = 2 + 2;
+        assert_eq!(result, 4);
+    }
+    // todo test all parsing funcitons
 }
