8b65490bd4a669945c8139a3d25ad4c53fc866b2
Joe Goodman <79279956+TheDinner22@users.noreply.github.com>
2023-01-15 18:20:13 -0500

    fmt

--- src/lib.rs
+++ src/lib.rs
@@ -1,35 +1,38 @@
 mod easy_html;
 
 mod tcp_server {
-    use std::{net::{TcpListener, TcpStream}, io::{self, Read, Write}};
+    use std::{
+        io::{self, Read, Write},
+        net::{TcpListener, TcpStream},
+    };
 
     // use crate::easy_html;
 
-    pub fn try_start(port: u16) -> std::io::Result<TcpListener>{
+    pub fn try_start(port: u16) -> std::io::Result<TcpListener> {
         let socket_str = String::from("127.0.0.1:") + &port.to_string();
 
         TcpListener::bind(socket_str)
     }
 
     /// # read a TcpStream and return its contents as a Vec<String>
-    /// 
+    ///
     /// This function uses a std::io::BufRead to read from a TcpStream.
-    /// 
+    ///
     /// ## what is different from std::io::BufRead.lines()?
-    /// 
+    ///
     /// There is some internal logic which stops reading from the stream
     /// once the http request has been fully recived.
-    /// 
+    ///
     /// In short, calling the `lines` method will continue reading until
     /// the connection closes (or its told to stop reading). This function
     /// handles telling the `lines` method to stop reading.
-    /// 
+    ///
     /// ## panics
-    /// 
+    ///
     /// This function should never panic
-    /// 
+    ///
     /// ## errors
-    /// 
+    ///
     /// This function could return a std::io::Error.
     /// That means that the error occured when reading the contents of the stream.
     ///
@@ -46,31 +49,26 @@ mod tcp_server {
 
             if bytes_read == BUFFER_SIZE {
                 continue;
-            }
-            else {
+            } else {
                 break Ok(bytes);
             }
         }
-
-        
     }
-
-
 }
 
 pub mod server {
-    use std::net::{TcpListener, TcpStream};
     use std::io::{self, Write};
+    use std::net::{TcpListener, TcpStream};
 
     use super::easy_html;
     use super::tcp_server;
 
     pub struct Server {
-        listener: TcpListener
+        listener: TcpListener,
     }
 
     impl Server {
-        pub fn try_start(port: u16) -> std::io::Result<Self>{
+        pub fn try_start(port: u16) -> std::io::Result<Self> {
             let listener = tcp_server::try_start(port)?;
 
             let server = Server { listener };
@@ -100,17 +98,15 @@ pub mod server {
 
             Ok(())
         }
-        
 
         pub fn add_route<F>(&mut self, _route: &str, _f: F)
         where
-            F: Fn(easy_html::Request) -> easy_html::Response
+            F: Fn(easy_html::Request) -> easy_html::Response,
         {
             todo!()
         }
     }
 }
-
 
 #[cfg(test)]
 mod tests {
