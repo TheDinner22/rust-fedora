ec7a2f6a5bcd9b58322872f25e1e09d1893a2730
Joe Goodman <79279956+TheDinner22@users.noreply.github.com>
2023-01-22 15:41:17 -0500

    updated imports but now private issue

--- src/easy_html/tests.rs
+++ src/easy_html/tests.rs
@@ -1,27 +1,30 @@
 // through learning and other stuff I realized a lot of these should be doc tests!
 
-use super::{Request, Method::{*, self}};
+use super::{
+    method::Method::{self, *},
+    request::Request,
+};
 use std::collections::HashMap;
 
 // helper function to construct hashmaps from keys
 fn new_map<T, U>(pairs: Vec<(T, U)>) -> HashMap<T, U>
 where
     T: std::hash::Hash + std::cmp::Eq,
-    U: std::hash::Hash
+    U: std::hash::Hash,
 {
     pairs.into_iter().collect()
 }
 
 // attempt to parse the method from a string
 #[test]
-fn parse_method_works(){
+fn parse_method_works() {
     let inputs = ["get", "post", "put", "delete", "POST", "GeT"];
     let expected_outputs = vec![Get, Post, Put, Delete, Post, Get];
 
     // map inputs to outputs
     let outputs: Vec<Method> = inputs
         .into_iter()
-        .map( |input| Request::parse_method(input).unwrap() )
+        .map(|input| Request::parse_method(input).unwrap())
         .collect();
 
     assert_eq!(outputs, expected_outputs);
@@ -29,26 +32,31 @@ fn parse_method_works(){
 
 // pass bad input to be parsed
 #[test]
-fn parse_method_errs_as_expected(){
-    let inputs = ["get  ", "pst", "534378456jhkdfhks", "de lete", "P POST", "GeTPost"];
-    
+fn parse_method_errs_as_expected() {
+    let inputs = [
+        "get  ",
+        "pst",
+        "534378456jhkdfhks",
+        "de lete",
+        "P POST",
+        "GeTPost",
+    ];
+
     // map inputs to outputs
     let outputs: Vec<bool> = inputs
         .into_iter()
-        .map( |input| Request::parse_method(input).is_err() )
+        .map(|input| Request::parse_method(input).is_err())
         .collect();
 
     // every element in outputs should be true because
     // every input should have caused an error
-    let is_all_true = outputs
-        .into_iter()
-        .all(|item| item);
+    let is_all_true = outputs.into_iter().all(|item| item);
 
     assert_eq!(is_all_true, true);
 }
 
 #[test]
-fn parse_url_works(){
+fn parse_url_works() {
     let inputs = vec![
         "",
         "/",
@@ -69,14 +77,14 @@ fn parse_url_works(){
 
     let outputs: Vec<(&str, &str)> = inputs
         .into_iter()
-        .map( |url| Request::parse_url(url) )
+        .map(|url| Request::parse_url(url))
         .collect();
 
     assert_eq!(outputs, expected_outputs);
 }
 
 #[test]
-fn parse_query_params_works(){
+fn parse_query_params_works() {
     let inputs = [
         "name=joe",
         "name=1234&age=12",
@@ -85,14 +93,18 @@ fn parse_query_params_works(){
         "123=347",
         "=123 ",
         " 123=",
-        "=123&123="
+        "=123&123=",
     ];
 
     let expected_outputs = vec![
         new_map(vec![("name", "joe")]),
         new_map(vec![("name", "1234"), ("age", "12")]),
         new_map(vec![("name", "***")]),
-        new_map(vec![("age", "123"), ("name", "456"), ("height", " =tall= ")]),
+        new_map(vec![
+            ("age", "123"),
+            ("name", "456"),
+            ("height", " =tall= "),
+        ]),
         new_map(vec![("123", "347")]),
         new_map(vec![("", "123 ")]),
         new_map(vec![(" 123", "")]),
@@ -101,14 +113,14 @@ fn parse_query_params_works(){
 
     let outputs: Vec<HashMap<&str, &str>> = inputs
         .into_iter()
-        .map(|input| Request::parse_query_string(input) )
+        .map(|input| Request::parse_query_string(input))
         .collect();
 
     assert_eq!(outputs, expected_outputs);
 }
 
 #[test]
-fn parse_query_params_returns_empty_map_on_invlaid_input(){
+fn parse_query_params_returns_empty_map_on_invlaid_input() {
     let inputs = [
         "&&&&&&&&&&",
         "some invalid as heck input",
@@ -132,7 +144,7 @@ fn parse_query_params_returns_empty_map_on_invlaid_input(){
 }
 
 #[test]
-fn parse_query_params_ignores_invalid_input(){
+fn parse_query_params_ignores_invalid_input() {
     let inputs = [
         "valid=part&invalid-part",
         "invalid-part&valid=part",
@@ -162,7 +174,7 @@ fn parse_query_params_ignores_invalid_input(){
 }
 
 #[test]
-fn parse_query_params_ignores_duplicate_input(){
+fn parse_query_params_ignores_duplicate_input() {
     // this test assumes
     // that the last duplicate given will be the value
     // assigned to the hashmap
@@ -197,7 +209,7 @@ fn parse_query_params_ignores_duplicate_input(){
 }
 
 #[test]
-fn parse_http_ver_works(){
+fn parse_http_ver_works() {
     let inputs = [
         "HTTP/1.1",
         "HTTP/1.2",
@@ -213,43 +225,36 @@ fn parse_http_ver_works(){
                      // invalid http versions?!)
     ];
 
-    let expected_outputs: [u8; 10] = [ 1,2,3,4,5,6,7,8,9,0 ];
+    let expected_outputs: [u8; 10] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
 
     let outputs: Vec<u8> = inputs
         .into_iter()
-        .map(|input| Request::parse_http_ver(input).unwrap() )
+        .map(|input| Request::parse_http_ver(input).unwrap())
         .collect();
 
     assert_eq!(outputs, expected_outputs);
 }
 
 #[test]
-fn parse_http_ver_errors_as_expected(){
+fn parse_http_ver_errors_as_expected() {
     // three error conditions
     // 1. doesn't start w "HTTP/1."
     // 2. the last character is not an integer
 
     let inputs = [
-        "HTTP/1.",
-        "hTTP/1.",
-        "HTTP/2.",
-        "HTTP/2",
-        "HTTP/1.a",
-        "hTTP/1.a",
-        "HTTP/1. ",
-        "HTTP/.1",
+        "HTTP/1.", "hTTP/1.", "HTTP/2.", "HTTP/2", "HTTP/1.a", "hTTP/1.a", "HTTP/1. ", "HTTP/.1",
     ];
 
     // the map will panic if an error is not returned
     let _outputs: Vec<_> = inputs
         .into_iter()
-        .map( |input| Request::parse_http_ver(input).unwrap_err() )
+        .map(|input| Request::parse_http_ver(input).unwrap_err())
         .collect(); // need this or the iterator isnt run
 }
 
 #[test]
-fn parse_head_works(){
-    // fails rn because I want 
+fn parse_head_works() {
+    // fails rn because I want
     // the parse header function to
     // ignore case on keys, but it currently does not
 
@@ -277,7 +282,7 @@ fn parse_head_works(){
 }
 
 #[test]
-fn parse_head_returns_empty_on_all_bad_input(){
+fn parse_head_returns_empty_on_all_bad_input() {
     let input = &vec![
         "some ignored first line",
         "HeaDer1 somevaluehere",
@@ -294,7 +299,7 @@ fn parse_head_returns_empty_on_all_bad_input(){
 }
 
 #[test]
-fn parse_head_ignores_bad_input(){
+fn parse_head_ignores_bad_input() {
     let input = &vec![
         "some ignored first line",
         "header1: somevaluehere",
@@ -319,7 +324,7 @@ fn parse_head_ignores_bad_input(){
 }
 
 #[test]
-fn parse_head_ignores_duplicate_input(){
+fn parse_head_ignores_duplicate_input() {
     let input = &vec![
         "some ignored first line",
         "header1: somevaluehere",
