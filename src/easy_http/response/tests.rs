965fb3133fb3e277c18dd91793299d2f7582a336
Joe Goodman <79279956+TheDinner22@users.noreply.github.com>
2023-01-29 13:50:47 -0500

    not passing test

--- src/easy_http/response/tests.rs
+++ src/easy_http/response/tests.rs
@@ -1,1 +1,31 @@
-// todo make tests here for the response code
+use super::*;
+
+#[test]
+fn into_bytes_works_as_expected() {
+    let headers = {
+        let mut map = HashMap::new();
+        map.insert("hi".to_owned(), "there".to_owned());
+        map.insert("this".to_owned(), "is a header".to_owned());
+
+        map
+    };
+
+    let body: Vec<u8> = "hi there! I am the body!".bytes().collect();
+
+    let response = Response::new(200, headers, body);
+
+    let expected_output: Vec<u8> =
+        "HTTP/1.1 200 OK\r\nhi: there\r\nthis: is a header\r\n\r\nhi there! I am the body!"
+            .bytes()
+            .collect();
+
+    let output = response.into_bytes();
+
+    // human readable
+    assert_eq!(
+        std::str::from_utf8(&expected_output),
+        std::str::from_utf8(&output)
+    );
+    // not human readable
+    assert_eq!(expected_output, output);
+}
