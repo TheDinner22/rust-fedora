1bfe5da73cdcf65efbe2121a76cb4caba813b568
Joe Goodman <79279956+TheDinner22@users.noreply.github.com>
2023-03-04 10:36:39 -0500

    removed public module

--- src/router/public/mod.rs
+++
@@ -1,8 +0,0 @@
-macro_rules! public {
-    ($path:expr) => {
-        // I want this to add this dir to file based routing
-        // however, that means I need to add some kind of router
-        // and or file based routing first!
-        todo!()
-    };
-}
